47229bfa89e4d451e2c9ca3e313d2c4e7779e86f
335482616@qq.com <335482616@qq.com>
2023-06-11 14:07:29 +0800

    1

---
+++ firmware/amovias/icode/config.h
@@ -0,0 +1,12 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+#include "main.h"
+#include "keymap.h"
+#define BRIGHTNESS_RATIO  0.47    //最大亮度
+#define LED_NUMS          61      //led数量
+#define THICKNESS         0.8     //色彩过渡
+#define ANIMATION_SPEED   20
+#define KEY_NUM           61
+#define KEY_ROW_NUM       7
+#define KEY_COL_NUM       14
+#endif
